29f71f08416dd6647c731ea26cdef76df890a06e
dsbrown1331 <dsbrown1331@gmail.com>
2025-01-07 15:10:47 -0700

    Update mountain_car_play.py

--- mountain_car_play.py
+++ mountain_car_play.py
@@ -1,6 +1,9 @@
 import gym
 import pygame
 from teleop import play
-mapping = {(pygame.K_LEFT,): 0, (pygame.K_RIGHT,): 2}
+mapping = {(pygame.K_LEFT,): 0,
+           (pygame.K_RIGHT,): 2,
+           (): 1  # Coast (no keys pressed)
+           }
 env = gym.make("MountainCar-v0",render_mode='single_rgb_array') 
 demos = play(env, keys_to_action=mapping)
