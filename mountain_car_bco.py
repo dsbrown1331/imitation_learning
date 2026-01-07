5a806c18f4e87eb8ea4950502146694c6a6c8828
dsbrown1331 <dsbrown1331@gmail.com>
2026-01-07 13:36:56 -0700

    Remove unused numpy import
    Removed unused numpy import from mountain_car_bco.py

--- mountain_car_bco.py
+++ mountain_car_bco.py
@@ -8,7 +8,6 @@ from teleop import collect_demos
 import torch
 from torch.optim import Adam
 import torch.nn as nn
-import numpy as np
 import torch.nn.functional as F
 from mountain_car_bc import collect_human_demos, torchify_demos, train_policy, PolicyNetwork, evaluate_policy
 
